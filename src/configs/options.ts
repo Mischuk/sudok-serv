fe684a9e0b7f624641d230b6743bd17756d7694a
Oleksandr Mishchuk <mischuk.alexander@gmail.com>
2023-06-23 20:50:15 +0300

    cor

--- src/configs/options.ts
+++ src/configs/options.ts
@@ -3,7 +3,11 @@ import { ADDRESS, CLIENT_PORT } from "../lib";
 
 const ioServerOptions: Partial<ServerOptions> = {
   cors: {
-    origin: [`http://localhost:${CLIENT_PORT}`, `http://${ADDRESS}:${CLIENT_PORT}`],
+    origin: [
+      `http://localhost:${CLIENT_PORT}`,
+      `http://${ADDRESS}:${CLIENT_PORT}`,
+      `https://sudok-serv.onrender.com/`,
+    ],
     methods: ["GET", "POST"],
   },
 };
